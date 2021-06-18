ceeb0eba57113a9957111a0639660b774b487945
Daron Yondem <daron@yondem.com>
2021-06-18 15:31:57 +0300

    Hospital portal preliminary design

--- Hands-on lab/lab-files/source-hospital-portal/contoso-web/Pages/Index.cshtml.cs
+++ Hands-on lab/lab-files/source-hospital-portal/contoso-web/Pages/Index.cshtml.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure;
+using Azure.Search.Documents;
+using Azure.Search.Documents.Indexes;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,14 +15,23 @@ namespace contoso_web.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration Configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            Configuration = configuration;
         }
 
         public void OnGet()
         {
+            var azureSearchKey = Configuration["AzureSearchKey"];
+            var azureSearchUrl = Configuration["AzureSearchUrl"];
+            var searchIndexName = "cosmosdb-index";
+
+            AzureKeyCredential credential = new(azureSearchKey);
+
+            SearchClient searchclient = new(new Uri(azureSearchUrl), searchIndexName, credential);
 
         }
     }
